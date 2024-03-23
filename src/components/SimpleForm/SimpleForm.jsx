b9e770dbccbde4ee834b30ff232493fc31ce0a29
Jubayer-K <jubayerkhan21@gmail.com>
2024-03-24 01:13:26 +0600

    added simple form

--- src/components/SimpleForm/SimpleForm.jsx
+++ src/components/SimpleForm/SimpleForm.jsx
@@ -1,10 +1,28 @@
 const SimpleForm = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.email.value);
+    console.log(event.target.phone.value);
+  };
+
   return (
     <div>
-      <form className="flex flex-col justify-center items-center text-center">
-        <input className="bg-gray-300 rounded-lg" type="text" />
-        <hr />
-        <input className="bg-slate-700 text-white p-2 rounded-lg" type="submit" value="Submit" />
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col justify-center items-center text-center"
+      >
+        <input className="bg-gray-300 rounded-lg" name="name" type="text" />
+        <br />
+        <input className="bg-gray-300 rounded-lg" name="email" type="email" />
+        <br />
+        <input className="bg-gray-300 rounded-lg" name="phone" type="tel" />
+        <br />
+        <input
+          className="bg-slate-700 text-white p-2 rounded-lg"
+          type="submit"
+          value="Submit"
+        />
       </form>
     </div>
   );
